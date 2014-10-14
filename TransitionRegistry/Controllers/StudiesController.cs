d47e12ac0db9dcc9425f4c9231781b6d6cfedfc1
gberger <guilherme.berger@gmail.com>
2014-10-13 22:35:05 -0400

    Scaffold Patients and Studies

---
+++ TransitionRegistry/Controllers/StudiesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TransitionRegistry.Models;
+
+namespace TransitionRegistry.Controllers
+{
+    public class StudiesController : ApiController
+    {
+        private TransitionRegistryContext db = new TransitionRegistryContext();
+
+        // GET: api/Studies
+        public IQueryable<Study> GetStudies()
+        {
+            return db.Studies;
+        }
+
+        // GET: api/Studies/5
+        [ResponseType(typeof(Study))]
+        public IHttpActionResult GetStudy(int id)
+        {
+            Study study = db.Studies.Find(id);
+            if (study == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(study);
+        }
+
+        // PUT: api/Studies/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutStudy(int id, Study study)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != study.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(study).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Studies
+        [ResponseType(typeof(Study))]
+        public IHttpActionResult PostStudy(Study study)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Studies.Add(study);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = study.Id }, study);
+        }
+
+        // DELETE: api/Studies/5
+        [ResponseType(typeof(Study))]
+        public IHttpActionResult DeleteStudy(int id)
+        {
+            Study study = db.Studies.Find(id);
+            if (study == null)
+            {
+                return NotFound();
+            }
+
+            db.Studies.Remove(study);
+            db.SaveChanges();
+
+            return Ok(study);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StudyExists(int id)
+        {
+            return db.Studies.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ TransitionRegistry/Models/Patient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TransitionRegistry.Models
+{
+    public enum Gender
+    {
+        Female,
+        Male
+    }
+
+    public enum ParticipantType
+    {
+        Pediatric,
+        Adult
+    }
+
+
+    public class Patient
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string MrnNumber { get; set; }
+        [Column(TypeName = "Date")]
+        public DateTime Birthday { get; set; }
+        public Gender Gender { get; set; }
+        public ParticipantType ParticipantType { get; set; }
+
+        public virtual ICollection<Study> Studies { get; set; }
+    }
+}
