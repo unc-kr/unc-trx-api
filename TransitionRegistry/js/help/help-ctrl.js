b9c44a14f07fd23eaa6b38dcb1752fb7ddc5e00b
gberger <guilherme.berger@gmail.com>
2014-12-06 02:26:47 -0500

    help section

--- TransitionRegistry/js/help/help-ctrl.js
+++ TransitionRegistry/js/help/help-ctrl.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('uncTrxApp')
+  .directive('anchor', function() {
+    return {
+      restrict: 'A',
+      link: function(scope, $elm, attrs) {
+        var idToScroll = attrs.anchor;
+        $elm.on('click', function() {
+          var target = document.getElementById(idToScroll);
+          window.scrollTo(target.offsetTop || target.clientTop, 0);
+        });
+      }
+    }
+  })
   .controller('HelpCtrl', function ($scope) {
-    
+
   });
